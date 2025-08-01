34a681b4768c10f0d8023d3a0935d0142a85ae07
Heitor Polidoro <heitor.polidoro@gmail.com>
2025-08-01 14:51:18 -0300

    feat(docs): enhance README with additional badges and compatibility details
    chore(metadata): update package description in `pyproject.toml` and `__init__.py`

--- ppipeline/__init__.py
+++ ppipeline/__init__.py
@@ -2,7 +2,7 @@ from .pipeline import Pipeline
 
 __name__ = "Polidoro Pipeline"
 __version__ = "0.1"
-__description__ = "Pipeline for Python"  # TODO
+__description__ = "A powerful Python library for parallel data processing through a sequence of steps."
 __author__ = "Heitor Luis Polidoro"
 
 __all__ = ["Pipeline"]
